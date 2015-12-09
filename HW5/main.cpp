cc1daaaa2cdadace3abb9c98bf9f0c7b225a32b0
joylibingyao <joylibingyao@gmail.com>
2015-12-09 00:00:02 -0500

    Initial Commit

---
+++ HW5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  HW5
+//
+//  Created by Bingyao Li on 12/9/15.
+//  Copyright © 2015 CMPSC122. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
